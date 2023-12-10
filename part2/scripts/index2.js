53c3c3d19cf9907e1894887f9ae90a09a1a92e35
SerovaKaterina <k.serova.k@yandex.ru>
2023-12-11 00:03:49 +0200

    add changes

--- part2/scripts/index2.js
+++ part2/scripts/index2.js
@@ -1,59 +1,120 @@
+// создание родительского/базового класса
 class Transport {
     constructor(type, price, brand, image) {
-        this.type = type;
-        this.price = price;
-        this.brand = brand;
-        this.image = image;
+        this.type = type,
+            this.price = price,
+            this.brand = brand,
+            this.image = image
     }
+
+    // создание метода для вывода инфо
     getInfo() {
-        return `Type: ${this.type}, brand: ${this.brand}`;
+        return this.type,
+        this.brand,
+        this.image;
     }
+
+    // создание метода для вывода цены
     getPrice() {
-        return `Price: ${this.price}`;
-    }
-    getImageSrc() {
-        return `Link to photo: ${this.image}`;
+        return this.price;
     }
 }
 
-//Создаем класс Car, который наследует от Transport
+
+// создание дочернего класса
 class Car extends Transport {
-    constructor(type, price, brand, image, doors) {
+    constructor(type, price, brand,image, doors) {
         super(type, price, brand, image);
         this.doors = doors;
     }
+
+    // вывод количества дверей
     getDoorsCount() {
-        return `Quantity doors: ${this.doors}`;
+        return this.doors;
     }
 }
 
-// Создаем класс Bike, который наследует от Transport
+
+// создание дочернего класса
 class Bike extends Transport {
     constructor(type, price, brand, image, maxSpeed) {
-		super(type, price, brand, image);
-		this.maxSpeed = maxSpeed;
-	}
+        super(type, price, brand,image, maxSpeed);
+        this.maxSpeed = maxSpeed;
+    }
+
+    // геттер для вывода максимальной скорости
     getMaxSpeed() {
-        return `Max speed: ${this.maxSpeed}`;
+        return this.maxSpeed;
     }
 }
-const vehicles = data.map((vehicle) => {
-	if (vehicle.type === 'car') {
-		return new Car(vehicle.type, vehicle.price, vehicle.brand, vehicle.image, vehicle.doors);
-	} else if (vehicle.type === 'bike') {
-		return new Bike(vehicle.type, vehicle.price, vehicle.brand, vehicle.image, vehicle.maxSpeed);
-	}
-});
-vehicles.forEach((vehicle) => {
-	console.log(vehicle.getInfo());
-	console.log(vehicle.getPrice());
-	console.log(vehicle.getImageSrc());
-	if (vehicle instanceof Car) {
-		console.log(vehicle.getDoorsCount());
-	}
-	if (vehicle instanceof Bike) {
-		console.log(vehicle.getMaxSpeed());
-	}
+
+
+// Создание массивов для машин и мотоциклов
+let cars = [];
+let bikes = [];
+
+// Перебор массива с данными по транспорту
+data.forEach((item) => {
+    let type = item.type;
+    let brand = item.brand;
+    let price = item.price;
+    let doors = item.doors;
+    let maxSpeed = item.maxSpeed;
+    let image = item.image;
+
+    if (type === 'car') {
+        cars.push({ type, price, brand, doors, image });
+
+        let newCar = new Car(type, price, brand, doors, image);
+        // console.log(newCar);
+    }
+
+    else if (type === 'bike') {
+        bikes.push({ type, price, brand, maxSpeed, image });
+
+        let newBike = new Bike(type, price, brand, maxSpeed, image);
+        // console.log(newBike);
+    }
+
 });
 
 
+// Функция для вставки инфо на экран
+function insertInfo(arr, containerId) {
+    // Создание блока для вставки инфо
+    let container = document.getElementById(containerId);
+
+    arr.forEach((item) => {
+        let div = document.createElement('div');
+        div.classList = 'transport';
+
+        let h2 = document.createElement('h2');
+        h2.classList = 'title';
+        h2.textContent = item.brand;
+
+        let img = document.createElement('img');
+        img.src = item.image;
+        img.alt = `${item.brand}`;
+
+        let p = document.createElement('p');
+        p.classList = 'main_info';
+        if (arr === cars) {
+            p.textContent = `Цена: ${item.price} рублей, количество дверей: ${item.doors}.`;
+        }
+        else {
+            p.textContent = `Цена: ${item.price}, максимальная скорость: ${item.maxSpeed} км/ч.`;
+        }
+
+        // Вывод инфо на экран
+        container.append(div);
+        div.append(h2);
+        div.append(img);
+        div.append(p);
+
+    });
+
+}
+
+// Вызов функции для каждого массива
+insertInfo(cars, "carsContainer");
+insertInfo(bikes, "bikesContainer");
